5a035486a9fee072d0ffdd369d275a500e43dbc3
cznungester13-svg <cznungester13@gmail.com>
2026-06-25 09:07:33 -0400

    Set up Express server with API routes

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 
+const express = require("express");
+const app = express();
+
+const productRoutes = require("./routes/products");
+const orderRoutes = require("./routes/orders");
+const userRoutes = require("./routes/users");
+
+app.use(express.json());
+
+// Routes
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Marketplace API running");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
